170ca444ca40bf61420eab0727364a157bb15622
Lotses <Lotses@mail.ru>
2024-01-24 14:00:01 +0300

    Task2 added with some code

---
+++ Lesson_1/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿int int_value1 = 5;
+int int_value2 = 6;
+
+int sum = int_value1 + int_value2;
+Console.WriteLine(sum);
+
+long long_value = 6;
+long sum2 = long_value + int_value1
+Console.WriteLine(sum2);
+
+double double_value = 5.5;
+double sum3 = int_value1 + double_value;
+Console.WriteLine(sum3);
+
+int product1 = int_value1 * int_value2;
+long product2 = long_value * int_value1;
+double product3 = double_value * int_value1;
+
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
