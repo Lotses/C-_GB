ae70f6462f92af0c7107821af9c860aa0746bdb4
Lotses <Lotses@mail.ru>
2024-01-24 13:47:19 +0300

    Task1 file modified. Some code added

--- Lesson_1/Task1/Program.cs
+++ Lesson_1/Task1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello world!");
+﻿//Console.WriteLine("Hello world!");
+
+int int_value = 100;
+double double_value = 1.55;
+char char_value = 's';
+bool bool_value = true;
+
+Console.WriteLine(int_value);
+Console.WriteLine(double_value);
+Console.WriteLine(char_value);
+Console.WriteLine(bool_value);
