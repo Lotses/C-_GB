0b712e2e37f7dca74a89e17aaad89ed33f98122b
Lotses <Lotses@mail.ru>
2024-01-30 17:28:03 +0300

    Task 4 added

---
+++ Lesson_1/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = 5;
+int b = 3;
+int c = 7;
+int d = 1;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (c > max)
+{
+    max = d;
+}
+System.Console.WriteLine(max);
